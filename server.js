51de5546fe724938edb1614ecdf2ea778b7d99c4
brian presta <brian.presta@gmail.comgit commit -m first>
2020-07-28 12:17:00 -0700

    set up query filters

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const {animals} = require('./data/animals.json')
+const app = express()
+
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            query.personalityTraits = [query.personalityTraits]
+        }
+        filteredResults = filteredResults
+        .filter(animal => query.personalityTraits.every(trait => animal.personalityTraits.includes(trait)))
+        
+    }
+    return filteredResults;
+  }
+app.get('/api/animals', (req,res) => {
+    let results = animals
+    if (req.query) {
+        results = filterByQuery(req.query,results)
+    }
+    res.json(results)
+})
+app.listen(3001,function(){
+    console.log('API server now on port 3001!')
+})
