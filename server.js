4a9c8ba0d646715ce54542bccc8ad2c6b3135045
brian presta <brian.presta@gmail.comgit commit -m first>
2020-07-30 11:01:30 -0700

    added front end resources

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3001;
 const app = express()
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(express.static('public'))
 function filterByQuery(query, animalsArray) {
   let filteredResults = animalsArray;
   if (query.diet) {
@@ -79,6 +80,9 @@ app.post('/api/animals', (req,res) =>  {
     res.json(animal)
 }
 });
+app.get("/",(req,res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
 app.listen(PORT,function(){
     console.log('API server now on port 3001!')
 });
